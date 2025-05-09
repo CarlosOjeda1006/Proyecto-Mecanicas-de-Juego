eaa1258b09969ce50aeb33892f21e28ba042700f
diddierbro <diddierbro0404@gmail.com>
2025-05-08 20:39:25 -0600

    Difficulty button Added
    Añadí botón de dificultad

--- 2doProject/Assets/Scripts/Pause.cs
+++ 2doProject/Assets/Scripts/Pause.cs
@@ -1,9 +1,13 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
+    public Button DifficultyButton;
     
+    public int difficulty = 2;
     void Start()
     {
         
@@ -22,5 +26,24 @@ public class Pause : MonoBehaviour
     {
         Debug.Log("Cerrando juego...");
         Application.Quit();
+    }
+
+    public void Difficulty()
+    {
+        if(difficulty == 1)
+        {
+            difficulty = 2;
+            DifficultyButton.GetComponentInChildren<TextMeshProUGUI>().text = "MEDIUM";
+        }
+        else if(difficulty == 2)
+        {
+            difficulty = 3;
+            DifficultyButton.GetComponentInChildren<TextMeshProUGUI>().text = "HARD";
+        }
+        else if(difficulty == 3)
+        {
+            difficulty = 1;
+            DifficultyButton.GetComponentInChildren<TextMeshProUGUI>().text = "EASY";
+        }
     }
 }
